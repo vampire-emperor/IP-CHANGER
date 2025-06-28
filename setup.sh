bffc60faf69b8eeb235342c084342598ff590e6f
thakur2309 <thakuralok210@gmail.com>
2025-06-28 16:58:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 # Created by Alok Thakur
 
 echo -e "\e[1;33m[+] Updating & Upgrading Termux Packages...\e[0m"
-pkg update -y && pkg upgrade -y
+apt update -y && apt upgrade -y
 
 echo -e "\e[1;33m[+] Installing tur-repo & Required Packages...\e[0m"
-pkg install tur-repo -y
-pkg install tor privoxy netcat-openbsd curl -y
+apt install tur-repo -y
+apt install tor privoxy netcat-openbsd curl -y
 
 echo -e "\e[1;32m[✓] All dependencies installed successfully!\e[0m"
